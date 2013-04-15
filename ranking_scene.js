7cb3364b2d5108b5b895efa2f500867d023b549a
Yuta Okano <yuta.okano@gmail.com>
2013-04-16 02:48:51 +0900

    ランキングデータの取得

--- ranking_scene.js
+++ ranking_scene.js
@@ -4,6 +4,8 @@
  * @extends Scene
  */
 var RankingScene = Class.create(Scene, {
+	_rankingBoard: null,
+	
 	/**
 	 * 初期化
 	 * @memberof RankingScene
@@ -11,6 +13,7 @@ var RankingScene = Class.create(Scene, {
 	 */
 	initialize: function() {
 		Scene.call(this);
+		this._rankingBoard = new RankingBoard();
 	},
 	
 	/**
@@ -19,7 +22,64 @@ var RankingScene = Class.create(Scene, {
 	 * @method
 	 */
 	onenter: function() {
+		this.addChild(this._rankingBoard);
+		this._rankingBoard.load();
 	}
 });
 
-var RankingBoard = Class.create();
+/**
+ * ランキング表示板
+ * @class
+ * @extends Group
+ */
+var RankingBoard = Class.create(Group, {
+	/**
+	 * コンストラクタ
+	 * @method
+	 * @memberof RankingBoard
+	 */
+	initialize: function() {
+		Group.call(this);
+		var background = new Sprite();
+		background.image = game.assets[img('ranking_background.png')];
+		background.width = background.image.width;
+		background.height = background.image.height;
+		this.addChild(background);
+		
+		this.x = 17;
+		this.y = 12;
+		this.width = background.width;
+		this.height = background.height;
+	},
+	
+	/**
+	 * ランキングデータのロード
+	 * @method
+	 * @memberof RankingBoard
+	 */
+	load: function() {
+		$.ajax('/getranking', {
+			data: {
+				kind: 'mogura',
+				limit: 10
+			},
+			async: false,
+			error: function() {
+				alert('通信エラーが発生したためタイトルへ戻ります');
+				game.changeScene(TitleScene);
+			},
+			success: function(data) {
+				console.log(data);
+			}
+		});
+	},
+	
+	/**
+	 * タッチされたときの処理
+	 * @method
+	 * @memberof RankingBoard
+	 */
+	ontouchstart: function() {
+		game.changeScene(TitleScene);
+	}
+});
